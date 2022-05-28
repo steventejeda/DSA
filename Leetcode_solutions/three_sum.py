d87a005c3278fc68af112847976af92c8e920d64
steventejeda <steventejeda@gmail.com>
2022-05-28 19:24:50 -0400

    solved problem

---
+++ Leetcode_solutions/three_sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        
+        for index, value in enumerate(nums):
+            if index > 0 and value == nums[index - 1]:
+                continue
+                
+            left, right = index + 1, len(nums) - 1
+            
+            while left < right:
+                threeSum = value + nums[left] + nums[right]
+                if threeSum > 0:
+                    right -= 1 
+                elif threeSum < 0: 
+                    left += 1
+                else:
+                    result.append([value, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        return result
